51d24bca5c7f73c80af55bceff1edd05272ffb48
Jaap Overeem <56724237+JaapOvereem@users.noreply.github.com>
2021-09-29 12:05:20 +0000

    rect loop added

--- script.js
+++ script.js
@@ -1,53 +1,71 @@
-class Rect{
-  constructor(){
+class Rect {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
 
+  drawRect() {
+    rect(this.x, this.y, this.w, this.h);
+    this.x += -3;
   }
 }
 
-var yVal; 
-var accel; 
-var velocity; 
-var mass; 
+var yVal;
+var accel;
+var velocity;
+var mass;
 var rects = [];
+
 
 function setup() {
   createCanvas(640, 360);
-  
-  yVal = 0;  
-  velocity = 0; 
-  mass = 50; 
+
+  yVal = 0;
+  velocity = 0;
+  mass = 50;
   floppy = loadImage("images/35-disk.png");
-  accel = mass * 0.01; 
-  randomNum1 = random(200,350);
-  randomNum2 = randomNum1 - 150;
-  rect1 = new Rect(320, -10, 50, randomNum2)
-  rect2 = new Rect(320, randomNum1, 50, 300)
-  rects.push(rect1);
-  rects.push(rect2);
+  accel = mass * 0.01;
 }
 
 
 function draw() {
   background(127);
-  fill(255,0,0);
-  
-  velocity += accel; 
+  fill(255, 0, 0);
+
+  velocity += accel;
   yVal += velocity;
-	image(floppy,width/2,yVal, mass+20,mass-10); 
-  rect(320, -10, 50, randomNum2);
-  rect(320, randomNum1, 50, 300);
-  
-  if (yVal > height - mass/2) {
+  image(floppy, width / 2, yVal, mass + 20, mass - 10);
+
+
+  if (yVal > height - mass / 2) {
     velocity *= -0.6;
-    yVal = height - mass/2;
+    yVal = height - mass / 2;
   }
+
+  if (frameCount % 85 == 0) {
+    console.log("add pipe");
+
+    randomHeight = random(height - 150);
+
+    rect1 = new Rect(700, 0, 50, randomHeight)
+    rect2 = new Rect(700, randomHeight + 150, 50, 300)
+
+    rects.push(rect1);
+    rects.push(rect2);
+  }
+
+  rects.forEach(p => p.drawRect());
 }
 
 
+
+
 function keyPressed(spacebar) {
-  yVal + 40;  
+  yVal + 40;
   // - mass gedeeld door 6 om bij elke mass een proportionele stuiter te krijgen
-  velocity = -mass / 6; 
+  velocity = -mass / 6;
 }
 
 
